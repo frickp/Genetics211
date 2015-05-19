f5b84739043397767c645e771e6c645fd7f418d6
frickp <peter.l.frick@gmail.com>
2015-05-19 15:57:21 -0700

    1st problem set is up and running

---
+++ PS1/array.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[34]:
+
+a=['glutamine','lysine','proline']
+
+
+# In[35]:
+
+print a[::-1]
+
+
+# In[36]:
+
+for i in range(len(a),0,-1):
+    print a[i-1]
+
+
+# In[37]:
+
+print a[2]; print a[1]; print a[0]
+
+
+# In[ ]:
+
+
+
